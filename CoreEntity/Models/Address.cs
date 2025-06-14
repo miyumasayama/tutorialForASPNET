ad53df6bb4353bfca687454c9e41b75609f4024d
miyumasayama <921ohagi@gmail.com>
2025-06-14 16:13:25 +0900

    [コメント追加]

--- CoreEntity/Models/Address.cs
+++ CoreEntity/Models/Address.cs
@@ -2,6 +2,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CoreEntity.Models;
 
+// 複合型
+// CompanyとEmployeeが重複して持つプロパティを複合型として切り出す
+// 主キーを持たない
 [ComplexType]
 public class Address
 {
